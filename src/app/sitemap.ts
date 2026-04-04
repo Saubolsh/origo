d64efdee6d637b41beedbebcd86bf18ce432b1fa
danik <dan1ar3110@gmail.com>
2026-04-04 14:25:12 +0500

    force-dynamic

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import { getCategories } from "@/entities/category/api";
 import { getProducts } from "@/entities/product/api";
 import { canonicalUrl } from "@/shared/lib/seo-url";
 
-export const dynamic = "force-static";
+export const dynamic = "force-dynamic";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const [categories, products] = await Promise.all([
